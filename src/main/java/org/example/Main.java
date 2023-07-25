69921d06321d9dfd320e609fdf2325119e4cdd58
Kyrylov Ivan <kivanval@gmail.com>
2023-07-25 23:46:25 +0300

    Refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         new CommandLine(new ConsoleController()).execute(args);
-
     }
 }
 
