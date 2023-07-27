5c0d76204f965703278ec4793c4235ba646b22d7
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-27 12:06:04 +0200

    to simplify generatePassword + English translate

--- src/main/java/controler/ConsoleController.java
+++ src/main/java/controler/ConsoleController.java
@@ -9,19 +9,19 @@ import java.io.FileNotFoundException;
 
 @Command(name = "user")
 public class ConsoleController implements Runnable {
-    @Option(names = "--lth", description = "Довжина пароля", required = true)
+    @Option(names = "--lth", description = "Length of password", required = true)
     private int length;
 
-    @Option(names = {"-wc", "--count"}, description = "Задати кількість слів")
+    @Option(names = {"-wc", "--count"}, description = "Words count")
     private int wordCount = 1;
 
-    @Option(names = {"-s", "--spec"}, description = "Розділити слова символом (вказати символ)")
+    @Option(names = {"-s", "--spec"}, description = "To split words some symbols (use symbol except 0)")
     private char special = 0;
 
-    @Option(names = {"-n", "--num"}, description = "Додати цифри")
+    @Option(names = {"-n", "--num"}, description = "Use numbers")
     private boolean numeric;
 
-    @Option(names = {"-u", "--upper"}, description = "Починати слова з великої літери")
+    @Option(names = {"-u", "--upper"}, description = "Begins with a capital letter")
     private boolean toUpperFirst;
 
 
@@ -48,23 +48,31 @@ public class ConsoleController implements Runnable {
 
     private void validate() {
         if (length < 3) {
-            System.out.println("Довжина пароля повинна бути мінімум 3 символи");
+            System.out.println("Minimal length must be 3");
+            System.exit(1);
+        }
+        if (wordCount < 1) {
+            System.out.println("Minimal count of word must be 1");
             System.exit(1);
         }
         if (!numeric && special == 0 && length / wordCount < 3) {
-            System.out.println("Довжина пароля коротка, щоб вмістити " + wordCount + " слів");
+            System.out.println("Length of password is short for" + wordCount + " words");
             System.exit(1);
         }
         if (numeric && special == 0 && (length - 1) / wordCount < 3) {
-            System.out.println("Довжина пароля коротка, щоб вмістити " + wordCount + " слів і цифри");
+            System.out.println("Length of password is short for " + wordCount + " words and numbers");
             System.exit(1);
         }
         if (!numeric && special != 0 && (length - wordCount + 1) / wordCount < 3) {
-            System.out.println("Довжина пароля коротка, щоб вмістити " + wordCount + " слів і символи");
+            System.out.println("Length of password is short for  " + wordCount + " words and symbols");
             System.exit(1);
         }
         if (numeric && special != 0 && (length - wordCount) / wordCount < 3) {
-            System.out.println("Довжина пароля коротка, щоб вмістити " + wordCount + " слів, символи і цифри");
+            System.out.println("Length of password is short for  " + wordCount + " words and symbols and numbers");
+            System.exit(1);
+        }
+        if (wordCount == 1 && special != 0) {
+            System.out.println("mast be minimum 2 words to split of symbols");
             System.exit(1);
         }
 
