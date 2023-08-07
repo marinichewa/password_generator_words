f47cc13966477732554509fdb1e43346e3dc5211
marinicheva <alin4ikmarin4ik@gmail.com>
2023-08-07 17:21:37 +0200

    test +2

--- src/test/java/org/example/controler/ConsoleControllerTest.java
+++ src/test/java/org/example/controler/ConsoleControllerTest.java
@@ -1,12 +1,20 @@
 package org.example.controler;
 
+import org.example.generate.GenerateRules;
+import org.example.generate.GenerateWord;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConsoleControllerTest {
 
-    @org.junit.jupiter.api.Test
-    void testRun() {
+    @Test
+    void runTest() {
+
+        GenerateWord word = new GenerateWord();
+        GenerateRules rules=new GenerateRules(6,0,'p',false,true);
+        assertEquals ("Minimal count of word must be 1",word.generate(rules));
+
     }
 }
